fa01bbc6bf783ba92155465f11342444260d52ed
YieldingDepression (YieldingCoder) <90570076+YieldingExploiter@users.noreply.github.com>
2022-01-27 09:15:07 +0100

    Update start.zsh

--- start.zsh
+++ start.zsh
@@ -1,10 +1,3 @@
 #!/bin/zsh
-chmod +x GetJava.sh
-./GetJava.sh
-chmod +x --recursive JavaDL
-cd server
-if [ ! -f Purpur.jar ]; then
-  curl --output purpur.jar https://api.purpurmc.org/v2/purpur/1.18.1/latest/download
-fi
-../JavaDL/Java/bin/java -Xmx4G -Xmn4G -jar Purpur.jar
-cd ..
+chmod +x start.sh
+./start.sh
